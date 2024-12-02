fe15525ccd5f9b77e3f8abf8e4a6595260b01dbe
Felipe Santos <felipe.santos@qualyteam.com.br>
2024-12-02 11:47:20 -0300

    Adjust: Refactor Document Details page

--- src/pages/document-details/index.jsx
+++ src/pages/document-details/index.jsx
@@ -24,62 +24,66 @@ export const DocumentDetails = () => {
         api.get(`/documents/${params.id}`)
             .then(response => setDocument(response.data))
             .catch(error => console.log(error));
-        }, []);
+    }, []);
 
     return (
         <div>
             <PageHeader
                 title="Document Details"
             />
-            <PageContent>
-                <div className="document-details-container">
-                    <div className="document-info">
-                        <label htmlFor="code">Code:</label>
-                        <span id="code">{document?.code}</span>
-                    </div>
-
-                    <div className="document-info">
-                        <label htmlFor="title">Title:</label>
-                        <span id="title">{document?.title}</span>
-                    </div>
-
-                    <div className="document-info">
-                        <label htmlFor="release-date">Release Date:</label>
-                        <span id="release-date">{document["release-date"]}</span>
-                    </div>
-
-
-                    <div className="document-info">
-                        <label htmlFor="published">Published:</label>
-                        <span id="published">{document?.published ? <FaCheck color='#008000'/> : <FaXmark color='#FF0000' />}</span>
-                    </div>
-
-
-                    <div className="document-info">
-                        <label htmlFor="active">Active:</label>
-                        <span id="active">{document?.active ? <FaCheck color='#008000'/> :  <FaXmark color='#FF0000' />}</span>
-                    </div>
-
-                    {
-                        document?.processes && (
+            {
+                document && (
+                    <PageContent>
+                        <div className="document-details-container">
                             <div className="document-info">
-                                <label>Processes:</label>
-                                <ul>
-                                {
-                                    document.processes && document.processes.map(process => (
-                                        <li key={process.id}>{process.name}</li>
-                                    ))
-                                }
-                                </ul>
+                                <label htmlFor="code">Code:</label>
+                                <span id="code">{document.code}</span>
                             </div>
-                        )
-                    }
-                    
-                    <div className="back-btn-wrapper">
-                        <Button className="back-masterlist-btn" title="Back" onClick={handleBackToMasterList}/>
-                    </div>
-                </div>
-            </PageContent>
+
+                            <div className="document-info">
+                                <label htmlFor="title">Title:</label>
+                                <span id="title">{document.title}</span>
+                            </div>
+
+                            <div className="document-info">
+                                <label htmlFor="release-date">Release Date:</label>
+                                <span id="release-date">{document["release-date"]}</span>
+                            </div>
+
+
+                            <div className="document-info">
+                                <label htmlFor="published">Published:</label>
+                                <span id="published">{document.published ? <FaCheck color='#008000' /> : <FaXmark color='#FF0000' />}</span>
+                            </div>
+
+
+                            <div className="document-info">
+                                <label htmlFor="active">Active:</label>
+                                <span id="active">{document.active ? <FaCheck color='#008000' /> : <FaXmark color='#FF0000' />}</span>
+                            </div>
+
+                            {
+                                document.processes && (
+                                    <div className="document-info">
+                                        <label>Processes:</label>
+                                        <ul>
+                                            {
+                                                document.processes && document.processes.map(process => (
+                                                    <li key={process.id}>{process.name}</li>
+                                                ))
+                                            }
+                                        </ul>
+                                    </div>
+                                )
+                            }
+
+                            <div className="back-btn-wrapper">
+                                <Button className="back-masterlist-btn" title="Back" onClick={handleBackToMasterList} />
+                            </div>
+                        </div>
+                    </PageContent>
+                )
+            }
         </div>
     );
 }
